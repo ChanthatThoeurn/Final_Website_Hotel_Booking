2b722a99e0609c15047fdb8171e4443e944259f3
Chanthat <kokazono380@gmail.com>
2025-11-18 10:07:50 +0700

    update

---
+++ src/userProfile/user.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-primary font-khmer">សួស្តី</p>
+      <p className="text-secondary font-English">Hello</p>
+    </div>
+  );
+}
